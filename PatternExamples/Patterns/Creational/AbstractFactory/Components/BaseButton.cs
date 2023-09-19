ec375d3927ddfb4d0d742118be4995db326bcd04
Jan Zeman <jan.zeman@exon.cz>
2023-09-19 19:07:46 +0200

    [Feature]: AbstractFactory

--- PatternExamples/Patterns/Creational/AbstractFactory/Components/BaseButton.cs
+++ PatternExamples/Patterns/Creational/AbstractFactory/Components/BaseButton.cs
@@ -5,4 +5,30 @@
         protected string Content;
         public abstract void OnClick();
     }
+
+    public class WinButton : BaseButton
+    {
+        public WinButton()
+        {
+            Content = "WindowsButton";
+        }
+
+        public override void OnClick()
+        {
+            Console.WriteLine(Content);
+        }
+    }
+
+    public class LinuxButton : BaseButton
+    {
+        public LinuxButton()
+        {
+            Content = "LinuxButton";
+        }
+
+        public override void OnClick()
+        {
+            Console.WriteLine(Content);
+        }
+    }
 }
